46e135906725e09b66862494b3d054f50e9561db
David Vigh <josephs.david.b@gmail.com>
2024-10-14 13:06:04 -0500

    adding firenvim

--- init.lua
+++ init.lua
@@ -230,6 +230,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { 'glacambre/firenvim', build = ':call firenvim#install(0)' },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
