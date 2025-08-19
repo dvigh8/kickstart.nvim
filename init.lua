3d2e96ca6f793bda19b579eaa1ef13bb5ea27e85
David Vigh <david.vigh@aviditybio.com>
2025-08-19 08:44:37 -0500

    update to fix Control y

--- init.lua
+++ init.lua
@@ -864,6 +864,7 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ['<C-y>'] = {}, -- Disable C-y to let Supermaven use it
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
