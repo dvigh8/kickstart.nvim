29697caf2679832746b1e918823badb9718a9271
David Vigh <12058218+dvigh8@users.noreply.github.com>
2026-01-08 16:00:10 -0600

    update focus on toggle

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -9,7 +9,13 @@ return {
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
-      -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+      provider = {
+        snacks = {
+          win = {
+            enter = true,
+          },
+        },
+      },
     }
 
     -- Required for `opts.events.reload`.
